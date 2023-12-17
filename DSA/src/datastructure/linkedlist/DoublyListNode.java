1356883664f19373ccd1f01c71b0cb6dd752bf75
bhojakajay <bhojakab@gmail.com>
2023-12-18 00:28:28 +0530

    DSAMadeEasy: created DoublyListNode.

---
+++ DSA/src/datastructure/linkedlist/DoublyListNode.java
@@ -0,0 +1,30 @@
+package datastructure.linkedlist;
+
+public class DoublyListNode<T> {
+	private T data;
+	private DoublyListNode<T> next;
+	private DoublyListNode<T> previous;
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	public DoublyListNode<T> getNext() {
+		return next;
+	}
+	public void setNext(DoublyListNode<T> next) {
+		this.next = next;
+	}
+	public DoublyListNode<T> getPrevious() {
+		return previous;
+	}
+	public void setPrevious(DoublyListNode<T> previous) {
+		this.previous = previous;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getData().toString();
+	}
+}
