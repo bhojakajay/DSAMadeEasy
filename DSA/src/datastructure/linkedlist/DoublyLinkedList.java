3cc9894dd4e51bd1fb26492f761a0b0b267b7735
bhojakajay <bhojakab@gmail.com>
2023-12-19 02:40:18 +0530

    DSAMadeEasy : remove node operations and size field bug resolution while inserting elements.

--- DSA/src/datastructure/linkedlist/DoublyLinkedList.java
+++ DSA/src/datastructure/linkedlist/DoublyLinkedList.java
@@ -54,11 +54,14 @@ public class DoublyLinkedList<T> {
 		if(index<0) {
 			throw new IndexOutOfBoundsException();
 		}else if(headNode==null && tailNode==null && index>0) {
+			size=0;
 			throw new IndexOutOfBoundsException();
 		}else if(index==0) {
 			insertAtBeginning(item);
+			return;
 		}else if(index == size()) {
 			insertAtEnd(item);
+			return;
 		}else {
 			DoublyListNode<T> current = headNode;
 			while(index>1) {
@@ -68,15 +71,81 @@ public class DoublyLinkedList<T> {
 			DoublyListNode<T> node = new DoublyListNode<T>(item);
 			node.setNext(current.getNext());
 			current.setNext(node);
+			size++;
 		}
-		size++;
 	}
 	
 	public int size() {
 		int size = this.size;
 		return size;
 	}
+
+	public void removeFirst() {
+		if(headNode==null) {
+			size=0;
+			return;
+		}
+		if(headNode==tailNode) {
+			headNode = tailNode = null;
+			size=0;;
+			return;
+		}
+		DoublyListNode<T> currentHead = headNode;
+		headNode = currentHead.getNext();
+		headNode.setPrevious(null);
+		currentHead.setNext(null);
+		size--;
+	}
 	
+	public void removeLast() {
+		if(headNode==null) {
+			size = 0;
+			return;
+		}
+		if(headNode==tailNode) {
+			headNode = tailNode = null;
+			size=0;
+			return;
+		}
+		
+		DoublyListNode<T> current = headNode;
+		DoublyListNode<T> prev = headNode;
+		while(current.getNext()!=null) {
+			prev=current;
+			current=current.getNext();
+		}
+		prev.setNext(null);
+		current.setPrevious(null);
+		current = null;
+		tailNode = prev;
+		size--;
+	}
+	
+	public void removeIndex(int index) {
+		if(index<0 || index>=size) {
+			throw new IndexOutOfBoundsException();
+		}else if(index==0) {
+			removeFirst();
+			return;
+		}else if(index==size-1) {
+			removeLast();
+			return;
+		}else {
+			DoublyListNode<T> indexedNode = headNode;
+			DoublyListNode<T> prevNode = headNode;
+			while(index>0) {
+				prevNode = indexedNode;
+				indexedNode = indexedNode.getNext();
+				index--;
+			}
+			prevNode.setNext(indexedNode.getNext());
+			prevNode.getNext().setPrevious(prevNode);
+			indexedNode.setNext(null);
+			indexedNode.setPrevious(null);
+			indexedNode = null;
+			size--;
+		}
+	}
 	@Override
 	public String toString() {
 		DoublyListNode<?> currentNode = this.headNode;
@@ -108,7 +177,25 @@ public class DoublyLinkedList<T> {
 		System.out.println(linkedList);
 		linkedList.insertAtIndex("II",2);
 		System.out.println(linkedList);
-		linkedList.insertAtIndex("JJ",-1);
+		/* linkedList.insertAtIndex("JJ",-1); */
+		linkedList.removeLast();
 		System.out.println(linkedList);
+		linkedList.removeLast();
+		System.out.println(linkedList);
+		linkedList.removeLast();
+		System.out.println(linkedList);
+		linkedList.removeFirst();
+		System.out.println(linkedList);
+		linkedList.removeIndex(3);
+		System.out.println(linkedList);
+		linkedList.removeIndex(3);
+		System.out.println(linkedList);
+		linkedList.removeIndex(1);
+		System.out.println(linkedList);
+		/*
+		 * linkedList.removeIndex(); System.out.println(linkedList);
+		 * linkedList.removeLast(); System.out.println(linkedList);
+		 * linkedList.removeFirst(); System.out.println(linkedList);
+		 */
 	}
 }
